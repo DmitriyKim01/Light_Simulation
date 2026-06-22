e937a8860bf4412ce31c920bc3243c6cce699462
Dmitriy Kim <dmitriykim.dev@protonmail.com>
2026-06-22 17:08:43 -0400

    feat(light): add light shader and second cube light source

--- src/shader/OpenGLShader.cpp
+++ src/shader/OpenGLShader.cpp
@@ -117,6 +117,11 @@ void OpenGLShader::setVec2(const std::string& name, float x, float y) const
 	glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
 }
 
+void OpenGLShader::setVec3(const std::string& name, float x, float y, float z) const
+{
+	glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+}
+
 void OpenGLShader::setMat4(const std::string& name, const glm::mat4& mat) const
 {
 	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
